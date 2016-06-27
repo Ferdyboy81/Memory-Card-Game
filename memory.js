0d281f481c651cf9011c42784be19aeae06b4c9f
Fernand Tan <ferdyboy81@gmail.com>
2016-06-27 11:13:24 -0700

    added var & if statement

---
+++ memory.js
@@ -0,0 +1,12 @@
+// Creating a variable called cards that will hold the
+//elements of array
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if(cardOne == cardThree) {
+   alert("You found a match!.")
+
+}
